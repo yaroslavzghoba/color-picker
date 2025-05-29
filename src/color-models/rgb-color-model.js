8148169c968893c8890f04d60ee63326c10d0130
Yaroslav Zghoba <yaroslavzghoba@gmail.com>
2025-05-29 08:55:22 +0200

    Implemented converting HEX to RGB and RGB to HEX

---
+++ src/color-models/rgb-color-model.js
@@ -0,0 +1,50 @@
+import { HexUtils } from '../utils/hex-utils'
+
+
+// Represents a color using red, green, and blue components.
+export class RgbColorModel {
+
+    red = 0;    // The color's red component.
+    green = 0;  // The color's green component.
+    blue = 0;   // The color's blue component.
+    hex;        // A hexadecimal color code.
+
+    constructor() {
+        // Init the HEX using RGB values.
+        this.hex = calculateHex(this.red, this.green, this.blue);
+    }
+    
+    // Define a color using hexadecimal color code.
+    fromHex(hex) {
+        HexUtils.validateNumHex(hex);
+            
+        this.red = (hex >> 16) & 255;
+        this.green = (hex >> 8) & 255;
+        this.blue = hex & 255;
+        this.hex = hex;
+    };
+
+    // Define a color using values of red, green, and blue.
+    fromRgb(r, g, b) {
+        validateColorValue(r);
+        validateColorValue(g);
+        validateColorValue(b);
+
+        this.red = r;
+        this.green = g;
+        this.blue = b;
+        this.hex = calculateHex(r, g, b);
+    };
+};
+
+// Check if the value is a number between 0 and 255.
+function validateColorValue(value) {
+    if (typeof value !== 'number' || (value <= 0 || value >= 255) || isNaN(value)) {
+        throw new Error('Invalid color value. It should be a number between 0 and 255.');
+    };
+};
+
+// Convert a RGB color to a hexadecimal color code.
+function calculateHex(r, g, b) {
+    return (r << 16) | (g << 8) | b;
+};
--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import { HslColorModel } from './color-models/hsl-color-model'
+import { RgbColorModel } from './color-models/rgb-color-model';
 import { HexUtils } from './utils/hex-utils';
 
 
@@ -9,6 +10,9 @@ const hexInput = document.getElementById('hex-input');
 const hslHueInput = document.getElementById('hsl-h-input');
 const hslSaturationInput = document.getElementById('hsl-s-input');
 const hslLightnessInput = document.getElementById('hsl-l-input');
+const rgbRedInput = document.getElementById('rgb-r-input');
+const rgbGreenInput = document.getElementById('rgb-g-input');
+const rgbBlueInput = document.getElementById('rgb-b-input');
 
 // Change the selected color in the CSS variable.
 function changeSelectedCode(hexValue) {
@@ -30,16 +34,41 @@ function useHexToUpdateHsl(hex) {
     hslLightnessInput.value = hslColorModel.lightness;
 }
 
+// Update RGB input fields using a HEX code.
+function useHexToUpdateRgb(hex) {
+    const rgbColorModel = new RgbColorModel();
+    rgbColorModel.fromHex(HexUtils.stringToHex(hex));
+
+    rgbRedInput.value = rgbColorModel.red;
+    rgbGreenInput.value = rgbColorModel.green;
+    rgbBlueInput.value = rgbColorModel.blue;
+}
+
 // Update other color inputs and display a new color using HSL values.
 function onHslUpdated() {
-    const h = parseInt(document.getElementById('hsl-h-input').value);
-    const s = parseInt(document.getElementById('hsl-s-input').value);
-    const l = parseInt(document.getElementById('hsl-l-input').value);
+    const h = parseInt(hslHueInput.value);
+    const s = parseInt(hslSaturationInput.value);
+    const l = parseInt(hslLightnessInput.value);
     const hslColorModel = new HslColorModel();
     hslColorModel.fromHsl(h, s, l);
 
     const hex = HexUtils.hexToString(hslColorModel.hex);
     updateHex(hex);
+    useHexToUpdateRgb(hex);
+    changeSelectedCode(hex);
+}
+
+// Update other color inputs and display a new color using RGB values.
+function onRgbUpdated() {
+    const r = parseInt(rgbRedInput.value);
+    const g = parseInt(rgbGreenInput.value);
+    const b = parseInt(rgbBlueInput.value);
+    const rgbColorModel = new RgbColorModel();
+    rgbColorModel.fromRgb(r, g, b);
+
+    const hex = HexUtils.hexToString(rgbColorModel.hex);
+    updateHex(hex);
+    useHexToUpdateHsl(hex);
     changeSelectedCode(hex);
 }
 
@@ -47,6 +76,7 @@ function onHslUpdated() {
 function onHexUpdated() {
     const hex = hexInput.value;
     useHexToUpdateHsl(hex);
+    useHexToUpdateRgb(hex);
     changeSelectedCode(hex);
 };
 
@@ -65,4 +95,15 @@ hslSaturationInput.addEventListener('input', function() {
 });
 hslLightnessInput.addEventListener('input', function() {
     onHslUpdated()
+});
+
+// Set listeners to linsten for RGB values input
+rgbRedInput.addEventListener('input', function() {
+    onRgbUpdated()
+});
+rgbGreenInput.addEventListener('input', function() {
+    onRgbUpdated()
+});
+rgbBlueInput.addEventListener('input', function() {
+    onRgbUpdated()
 });
